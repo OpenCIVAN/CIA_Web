19cbce0dfd4ee19daccf2954e66af2ccc0ec0914
Elizabeth Bernhardt <ebernhardt11@gmail.com>
2025-11-23 10:00:23 -0500

    Adding stubs for collaboration panel

---
+++ src/ui/react/components/collaboration/ActivityPanel/index.js
@@ -0,0 +1,4 @@
+// src/ui/react/components/collaboration/ActivityPanel/index.js
+
+export { ActivityPanel, default } from "./ActivityPanel.jsx";
+export { useActivityFeed } from "./useActivityFeed.js";
---
+++ src/ui/react/components/collaboration/QuickAccess/index.js
@@ -0,0 +1,8 @@
+// src/ui/react/components/collaboration/QuickAccess/index.js
+
+export { RecordingTab } from "./RecordingTab.jsx";
+export { useRecordings } from "./useRecordings.js";
+
+// TODO: These will be added/enhanced
+// export { SettingsTab } from "./SettingsTab.jsx";
+// export { CursorsTab } from "./CursorsTab.jsx";
