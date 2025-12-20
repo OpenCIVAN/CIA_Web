39494953859413f66e8f52d7d884d0fda0a13479
Elizabeth Bernhardt <ebernhardt11@gmail.com>
2025-12-20 14:56:37 -0500

    Cleaning up

--- src/ui/react/components/navigation/RoomSelector/index.js
+++
@@ -1,4 +0,0 @@
-// src/ui/react/components/navigation/RoomSelector/index.js
-export { RoomSelector } from "./RoomSelector.jsx";
-export { default } from "./RoomSelector.jsx";
-export { useRoomSelector, useRooms } from "./RoomSelector.logic.js";
